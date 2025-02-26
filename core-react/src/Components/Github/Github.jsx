563212721553a004112ffcf5c1f0307a8253a82a
Prachi Pradiprao Rakhonde <prachi.rakhonde@acefone.com>
2025-02-26 11:46:55 +0530

    changes

--- core-react/src/Components/Github/Github.jsx
+++ core-react/src/Components/Github/Github.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
+import { useLoaderData } from 'react-router-dom'
+
 
 function Github() {
+  const data = useLoaderData();
+  // const [data,setData] = useState([]);
+  // useEffect(()=>{
+  //   fetch('https://api.github.com/users/prachirakhonde123')
+  //   .then(response => response.json())
+  //   .then(data => 
+  //     // console.log(data.followers),
+  //     setData(data)
+  //   )
+  // },[])
+
   return (
     <div className='text-center bg-gray-600 p-4 m-4 text-white text-3xl'>
-        Github Followers : 
+        Github Followers : {data.followers}
+        <img src={data.avatar_url} alt="Git Picture" width={300}/>
     </div>
   )
 }
 
 export default Github
+
+/* Another way is By using loader for that we need function */
+
+export const githubLoaderData = async () => {
+   const response = await fetch('https://api.github.com/users/prachirakhonde123')
+   return response.json();
+}
--- core-react/src/main.jsx
+++ core-react/src/main.jsx
@@ -8,7 +8,7 @@ import About from './Pages/About.jsx'
 import Contact from './Pages/Contact-Us.jsx'
 import './index.css'
 import { User } from './Components/index.js'
-import Github from './Components/Github/Github.jsx'
+import Github, { githubLoaderData } from './Components/Github/Github.jsx'
 // Method : 1--------------------------------------------
 // const router = createBrowserRouter ([
 //   {
@@ -38,10 +38,15 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout/>}>
       <Route path='' element={<Home/>}/>
-      <Route path='about' element={<About/>}/>
+      <Route path='about' element={<About/>}>
+         <Route path='blogs' element={<About/>}/>  
+      </Route>  
       <Route path='contact-us' element={<Contact/>}/>
       <Route path='user/:id' element={<User/>}/>
-      <Route path='github' element={<Github/>}/>
+      <Route 
+        loader = {githubLoaderData}  // for optimization
+        path='github' 
+        element={<Github/>}/>
     </Route>
   )
 )
@@ -52,3 +57,11 @@ createRoot(document.getElementById('root')).render(
     {/* <App /> */}
   </StrictMode>,
 )
+
+/*
+Nested Routing : -
+route will become : /about/blog
+ <Route path='about' element={<About/>}>
+    <Route path='blogs' element={<About/>}/>  
+ </Route>  
+*/
